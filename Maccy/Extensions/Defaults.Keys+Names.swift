d31b58386487b8c3de374f3b0f229d677191189b
kaigedong <dongkaige@gmail.com>
2026-06-10 13:46:49 +0800

    feat: show application icons by default

--- Maccy/Extensions/Defaults.Keys+Names.swift
+++ Maccy/Extensions/Defaults.Keys+Names.swift
@@ -57,7 +57,7 @@ extension Defaults.Keys {
   static let suppressClearAlert = Key<Bool>("suppressClearAlert", default: false)
   static let windowSize = Key<NSSize>("windowSize", default: NSSize(width: 450, height: 800))
   static let windowPosition = Key<NSPoint>("windowPosition", default: NSPoint(x: 0.5, y: 0.8))
-  static let showApplicationIcons = Key<Bool>("showApplicationIcons", default: false)
+  static let showApplicationIcons = Key<Bool>("showApplicationIcons", default: true)
   static let previewWidth = Key<CGFloat>("previewWidth", default: 400)
   static let betaUpdates = Key<Bool>("betaUpdates", default: false)
 }
